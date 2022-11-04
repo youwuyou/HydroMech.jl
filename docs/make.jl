16b7f86c6b1ae785c4ae7dfe0fe204fe2d6585b4
youwuyou <youwuyou@student.ethz.ch>
2022-11-04 15:43:29 +0100

    build up basic structure of the website

--- docs/make.jl
+++ docs/make.jl
@@ -7,8 +7,50 @@ makedocs(
          sitename = "HydroMech.jl",
          modules  = [HydroMech],
          pages=[
-                "Home" => "index.md"
-               ])
+                "Home" => "index.md",
+                "Getting started" => 
+                    [
+                            "Overview" => "overview.md",
+                            "Source code" => [ 
+                                                "Modules" => "modules.md",
+                                                "Types"   => "types.md"
+                                             ],
+                              
+                    ],
+                "Solvers" =>
+                    [
+                        "2D-Hydro-mechanical solver" => "2D-hydro-mechanical-solver.md",
+                        "3D-Hydro-mechanical solver" => "3D-hydro-mechanical-solver.md"
+                    ],
+                "Concepts" => 
+                    [
+                    "Pseudo Transient Method" => "pseudo-transient-method.md"
+                    ],
+                        
+                "Benchmarks" => "benchmarks.md",
+                "Visualization" => "visualization.md",          
+                "Troubleshooting" => "troubleshooting.md",
+                
+                "Reference" => 
+                    [
+                        "Computational earthquake physics" => "computational-earthquake-physics.md",
+                        "PTsolvers/JustRelax.jl" => 
+                                            [ "Workflow"    =>  "justrelax-workflow.md",
+                                              "Source code" => [
+                                                                    "Modules" => "justrelax-modules.md",
+                                                                    "Types"   => "justrelax-types.md"
+                                                                ],
+                                              "Miniapps"    => "justrelax-Miniapps.md",
+                                              "Testing"     => "justrelax-Testing.md"
+                                            ],
+                        "ParallelStencil" => "parallelstencil.md",
+                        "JuliaGeodynamics" => "juliageodynamics.md"
+                    ],
+ 
+                "License" => "license.md"
+                ]
+        
+       )
 deploydocs(;
     repo="github.com/youwuyou/HydroMech.jl",
 )
