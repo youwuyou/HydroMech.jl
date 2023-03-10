3cfec57af36831e89105a236a3c9e945274d9150
youwuyou <youwuyou@student.ethz.ch>
2023-03-10 00:14:42 -0800

    testing only the incompressible case

--- test/test_2D.jl
+++ test/test_2D.jl
@@ -19,20 +19,20 @@ include("../benchmark/incompressible/PorosityWave2D.jl")
 end;
 
 
-include("../benchmark/compressible/PorosityWave2D.jl")
+# include("../benchmark/compressible/PorosityWave2D.jl")
 
-# compressible solver
-# solving incompressible case by setting parameter 
-@testset "Reference test: PorosityWave2D_compressible" begin
-    # Testing information
-    printstyled("Time stepping: t_tot = 0.0005, Δt = 1e-5\n"; bold=true, color=:white)
+# # compressible solver
+# # solving incompressible case by setting parameter 
+# @testset "Reference test: PorosityWave2D_compressible" begin
+#     # Testing information
+#     printstyled("Time stepping: t_tot = 0.0005, Δt = 1e-5\n"; bold=true, color=:white)
     
-    Peff_ref = load("2D/com_Peff.jld")["data"]
-    Peff     = PorosityWave2D_compressible(;t_tot_=0.0005)
+#     Peff_ref = load("2D/com_Peff.jld")["data"]
+#     Peff     = PorosityWave2D_compressible(;t_tot_=0.0005)
 
-    I = sample(1:length(Peff_ref), 5, replace=false)
+#     I = sample(1:length(Peff_ref), 5, replace=false)
 
-    @testset "randomly chosen entries $i" for i in I
-        @test Peff_ref[i] ≈ Peff[i]
-    end
-end;
+#     @testset "randomly chosen entries $i" for i in I
+#         @test Peff_ref[i] ≈ Peff[i]
+#     end
+# end;
